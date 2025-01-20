05672490df76509f5dee8547676e9c8a6ed4c67d
Eui-chan Jung <78064289+euichan2322@users.noreply.github.com>
2025-01-20 22:31:10 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+export const config = {
+    serverURL: 'https://bibimping.euichan.com/api/', //엔드포인트
+    //serverURL: 'http://127.0.0.1:8080/api/', //엔드포인트
+    //serverURL: 'http://152.67.222.171:8080/api/', //엔드포인트
+    potal:
+      'https://www.jnu.ac.kr/WebApp/web/HOM/COM/Board/board.aspx?boardID=5&bbsMode=list&cate=0&page=1&renderID=0.8621697344888588',
+    haksa:
+      'https://www.jnu.ac.kr/WebApp/web/HOM/COM/Board/board.aspx?boardID=5&bbsMode=list&cate=5&page=1&renderID=0.17835184060946108',
+    janghack:
+      'https://www.jnu.ac.kr/WebApp/web/HOM/COM/Board/board.aspx?boardID=5&bbsMode=list&cate=8&page=1&renderID=0.8960634763271347',
+    chjin:
+      'https://www.jnu.ac.kr/WebApp/web/HOM/COM/Board/board.aspx?boardID=5&bbsMode=list&cate=7&page=1&renderID=0.3028636217444065',
+    sojoong: 'https://www.swjnu.kr/www/notice/',
+    inhyuck:
+      'https://www.aicoss.kr/www/notice/?cate=%EC%A0%84%EB%82%A8%EB%8C%80%ED%95%99%EA%B5%90',
+    EAI: 'https://eaierc.jnu.ac.kr/community/notice/',
+    chahyuck: 'https://jnu.nccoss.kr/www/notice/',
+    heart: `<svg width="25" height="25" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M30.3919 6.72289C29.6471 5.97769 28.7627 5.38654 27.7893 4.98322C26.8159 4.5799 25.7726 4.37231 24.719 4.37231C23.6654 4.37231 22.6221 4.5799 21.6487 4.98322C20.6753 5.38654 19.7909 5.97769 19.0461 6.72289L17.5003 8.26873L15.9544 6.72289C14.4499 5.21834 12.4093 4.37309 10.2815 4.37309C8.15375 4.37309 6.11314 5.21834 4.60859 6.72289C3.10404 8.22744 2.25879 10.2681 2.25879 12.3958C2.25879 14.5236 3.10404 16.5642 4.60859 18.0687L17.5003 30.9604L30.3919 18.0687C31.1371 17.3239 31.7283 16.4395 32.1316 15.4661C32.5349 14.4927 32.7425 13.4494 32.7425 12.3958C32.7425 11.3422 32.5349 10.2989 32.1316 9.3255C31.7283 8.35212 31.1371 7.46775 30.3919 6.72289Z" stroke="#696969" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+  `,
+  };
